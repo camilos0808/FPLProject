719a297470f79cd4d18bad76ac87fc843d8bba25
Camilo <camilos-94@hotmail.com>
2022-10-23 14:21:25 +0100

    Endpoint.py file created

---
+++ _endpoints.py
@@ -0,0 +1,13 @@
+ENDPOINTS = {
+    '_General': 'bootstrap-static/',
+    '_Fixtures': 'fixtures/',
+    '_EventStatus': 'event-status/',
+    '_PlayerDetails': 'element-summary/%s/',  # Player's ID
+    '_GameWeekLive': 'event/%s/live/',  # GameWeek's ID
+    '_ManagerInfo': 'entry/%s/',  # Manager's ID
+    '_ManagerHistory': 'entry/%s/history',  # Manager's ID
+    '_LeagueStandings': 'leagues-classic/%s/standings',  # League's ID
+    '_ManagerTeams': 'entry/%s/event/%d/picks/',  # Manager's ID, GameWeek's ID
+    '_DreamTeam': 'dream-team/%s/',  # GameWeek's ID
+    '_SetPiece': 'team/set-piece-notes/',
+}
--- api.py
+++ api.py
@@ -1,80 +1,11 @@
 import requests
 import pandas as pd
-
-BASE = 'https://fantasy.premierleague.com/api/'
-
-
-def gameweek_live(gameweek_id) -> dict:
-    """
-    :param gameweek_id:
-    :return:
-    """
-    return requests.get(BASE + f'event/{gameweek_id}/live/').json()
-
-
-def manager_info(manager_id) -> dict:
-    """
-    :param manager_id:
-    :return:
-    """
-    return requests.get(BASE + f'entry/{manager_id}/').json()
-
-
-def manager_history(manager_id) -> dict:
-    """
-    :param manager_id:
-    :return:
-    """
-    return requests.get(BASE + f'entry/{manager_id}/history').json()
-
-
-def league_standings(league_id) -> dict:
-    """
-    :param league_id:
-    :return:
-    """
-    return requests.get(BASE + f'leagues-classic/{league_id}/standings').json()
-
-
-def manager_teams(manager_id, event_id) -> dict:
-    """
-    :param manager_id:
-    :param event_id:
-    :return:
-    """
-    return requests.get(BASE + f'entry/{manager_id}/event/{event_id}/picks/').json()
-
-
-def dream_team(event_id) -> dict:
-    """
-    :param event_id:
-    :return:
-    """
-    return requests.get(BASE + f'dream-team/{event_id}/').json()
-
-
-def set_piece() -> dict:
-    """
-    :return:
-    """
-    return requests.get(BASE + f'team/set-piece-notes/').json()
+from _endpoints import ENDPOINTS
 
 
 class FPLClient:
     BASE = 'https://fantasy.premierleague.com/api/'
-    ENDPOINTS = {
-        '_General': 'bootstrap-static/',
-        '_Fixtures': 'fixtures/',
-        '_EventStatus': 'event-status/',
-        '_PlayerDetails': 'element-summary/%s/',  # Player's ID
-        '_GameWeekLive':'event/%s/live/',  # GameWeek's ID
-        '_ManagerInfo':'entry/%s/',  # Manager's ID
-        '_ManagerHistory': 'entry/%s/history',  # Manager's ID
-        '_LeagueStandings': 'leagues-classic/%s/standings',  # League's ID
-        '_ManagerTeams': 'entry/%s/event/%d/picks/',  # Manager's ID, GameWeek's ID
-        '_DreamTeam': 'dream-team/%s/',  # GameWeek's ID
-        '_SetPiece': 'team/set-piece-notes/',
-    }
+    ENDPOINTS = ENDPOINTS
 
     def __init__(self):
         for key in FPLClient.ENDPOINTS.keys():
